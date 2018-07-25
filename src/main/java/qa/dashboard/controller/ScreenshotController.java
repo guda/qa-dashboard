e94cc6cbebdbc018232dff8b51caabf21fa9cd07
guda <amir.gudic@gmail.com>
2018-07-25 11:11:47 +0200

    Specific mapping, added favicon, removed web jars and used bootstrap.min.css directly

--- src/main/java/qa/dashboard/controller/ScreenshotController.java
+++ src/main/java/qa/dashboard/controller/ScreenshotController.java
@@ -3,7 +3,11 @@ package qa.dashboard.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import qa.dashboard.model.Screenshot;
 import qa.dashboard.repository.ScreenshotRepository;
@@ -11,42 +15,38 @@ import qa.dashboard.repository.ScreenshotRepository;
 @Controller
 public class ScreenshotController {
 
-    final
-    private ScreenshotRepository screenshotRepository;
+    private final ScreenshotRepository screenshotRepository;
 
     @Autowired
     public ScreenshotController(ScreenshotRepository screenshotRepository) {
         this.screenshotRepository = screenshotRepository;
     }
 
-    @RequestMapping("/screenshot")
+    @GetMapping("/screenshot")
     public String screenshot(Model model) {
         model.addAttribute("screenshots", screenshotRepository.findAll());
         return "screenshot";
     }
 
-    @RequestMapping("/screenshot-save")
+    @GetMapping("/screenshot-create")
+    public String screenshotCreate() {
+        return "screenshot-create";
+    }
+
+    @PostMapping("/screenshot-save")
     public String save(@RequestParam String screenshotName, @RequestParam String screenshotDesc, @RequestParam String screenshotUrl) {
         Screenshot screenshot = new Screenshot();
         screenshot.setScreenshotName(screenshotName);
         screenshot.setScreenshotDesc(screenshotDesc);
         screenshot.setScreenshotUrl(screenshotUrl);
 
-        ModelAndView mav = saveScreenshot(screenshot);
-
-        //saveScreenshot(screenshot).getView();
-        //screenshotRepository.save(screenshot);
+        ModelAndView mav = save(screenshot);
 
         return mav.getViewName();
     }
 
-    @RequestMapping("/screenshot-create")
-    public String screenshotCreate() {
-        return "screenshot-create";
-    }
-
     @PostMapping("/screenshot/create")
-    public ModelAndView saveScreenshot(@RequestBody Screenshot screenshot) {
+    public ModelAndView save(@RequestBody Screenshot screenshot) {
         screenshotRepository.save(screenshot);
 
         ModelAndView mav = new ModelAndView();
@@ -54,17 +54,15 @@ public class ScreenshotController {
         mav.setViewName("redirect:/screenshot-show/" + screenshot.getId());
 
         return mav;
-
-//        return new ResponseEntity<>(screenshot, HttpStatus.OK);
     }
 
-    @RequestMapping("/screenshot-show/{id}")
+    @GetMapping("/screenshot-show/{id}")
     public String show(@PathVariable String id, Model model) {
         model.addAttribute("screenshot", screenshotRepository.findById(id).get());
         return "screenshot-show";
     }
 
-    @RequestMapping("/screenshot-delete")
+    @GetMapping("/screenshot-delete")
     public String delete(@RequestParam String id) {
         Screenshot screenshot = screenshotRepository.findById(id).get();
         screenshotRepository.delete(screenshot);
@@ -72,13 +70,13 @@ public class ScreenshotController {
         return "redirect:/screenshot";
     }
 
-    @RequestMapping("/screenshot-edit/{id}")
+    @GetMapping("/screenshot-edit/{id}")
     public String edit(@PathVariable String id, Model model) {
         model.addAttribute("screenshot", screenshotRepository.findById(id).get());
         return "screenshot-edit";
     }
 
-    @RequestMapping("/screenshot-update")
+    @GetMapping("/screenshot-update")
     public String update(@RequestParam String id, @RequestParam String screenshotName, @RequestParam String screenshotDesc, @RequestParam String screenshotUrl) {
         Screenshot screenshot = screenshotRepository.findById(id).get();
         screenshot.setScreenshotName(screenshotName);
