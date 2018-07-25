682942d7cd2c1e1e853a5da76bf34f778f37a5eb
guda <amir.gudic@gmail.com>
2018-07-25 11:57:12 +0200

    Relocated screenshot views to specific folder and refactored accordingly

--- src/main/java/qa/dashboard/controller/ScreenshotController.java
+++ src/main/java/qa/dashboard/controller/ScreenshotController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import qa.dashboard.model.Screenshot;
 import qa.dashboard.repository.ScreenshotRepository;
 
 @Controller
+@RequestMapping("/screenshot")
 public class ScreenshotController {
 
     private final ScreenshotRepository screenshotRepository;
@@ -22,18 +24,18 @@ public class ScreenshotController {
         this.screenshotRepository = screenshotRepository;
     }
 
-    @GetMapping("/screenshot")
+    @GetMapping()
     public String screenshot(Model model) {
         model.addAttribute("screenshots", screenshotRepository.findAll());
-        return "screenshot";
+        return "screenshot/screenshot";
     }
 
-    @GetMapping("/screenshot-create")
+    @GetMapping("/create")
     public String screenshotCreate() {
-        return "screenshot-create";
+        return "screenshot/screenshot-create";
     }
 
-    @PostMapping("/screenshot-save")
+    @PostMapping("/save")
     public String save(@RequestParam String screenshotName, @RequestParam String screenshotDesc, @RequestParam String screenshotUrl) {
         Screenshot screenshot = new Screenshot();
         screenshot.setScreenshotName(screenshotName);
@@ -45,24 +47,24 @@ public class ScreenshotController {
         return mav.getViewName();
     }
 
-    @PostMapping("/screenshot/create")
+    @PostMapping("/create")
     public ModelAndView save(@RequestBody Screenshot screenshot) {
         screenshotRepository.save(screenshot);
 
         ModelAndView mav = new ModelAndView();
         mav.addObject("screenshot", screenshot);
-        mav.setViewName("redirect:/screenshot-show/" + screenshot.getId());
+        mav.setViewName("redirect:/screenshot/show/" + screenshot.getId());
 
         return mav;
     }
 
-    @GetMapping("/screenshot-show/{id}")
+    @GetMapping("/show/{id}")
     public String show(@PathVariable String id, Model model) {
         model.addAttribute("screenshot", screenshotRepository.findById(id).get());
-        return "screenshot-show";
+        return "screenshot/screenshot-show";
     }
 
-    @GetMapping("/screenshot-delete")
+    @GetMapping("/delete")
     public String delete(@RequestParam String id) {
         Screenshot screenshot = screenshotRepository.findById(id).get();
         screenshotRepository.delete(screenshot);
@@ -70,13 +72,13 @@ public class ScreenshotController {
         return "redirect:/screenshot";
     }
 
-    @GetMapping("/screenshot-edit/{id}")
+    @GetMapping("/edit/{id}")
     public String edit(@PathVariable String id, Model model) {
         model.addAttribute("screenshot", screenshotRepository.findById(id).get());
-        return "screenshot-edit";
+        return "screenshot/screenshot-edit";
     }
 
-    @GetMapping("/screenshot-update")
+    @GetMapping("/update")
     public String update(@RequestParam String id, @RequestParam String screenshotName, @RequestParam String screenshotDesc, @RequestParam String screenshotUrl) {
         Screenshot screenshot = screenshotRepository.findById(id).get();
         screenshot.setScreenshotName(screenshotName);
@@ -84,6 +86,6 @@ public class ScreenshotController {
         screenshot.setScreenshotUrl(screenshotUrl);
         screenshotRepository.save(screenshot);
 
-        return "redirect:/screenshot-show/" + screenshot.getId();
+        return "redirect:/screenshot/show/" + screenshot.getId();
     }
 }
